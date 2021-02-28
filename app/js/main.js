1b3af93c3d9e9ed4991cccd936217aaabf69f735
GoodGopnik <https://github.com/GoodGopnik>
2021-02-28 14:45:40 +0200

    Сделан первый блок без стилей и адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
-$(function(){
-
-
-
-  
-});
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
