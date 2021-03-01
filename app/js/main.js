a37dbfcd0c048180b7e20e7779b8c89f82af1daf
GoodGopnik <https://github.com/GoodGopnik>
2021-03-01 09:45:50 +0200

    Работаю над первым блоком

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
 $('.variable-width').slick({
     dots: true,
     infinite: true,
+    arrows: true,
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
-    variableWidth: true
+    variableWidth: true,
+    prevArrow: "<img src='../images/head/Line-left.png' class='prev' alt=''>",
+    nextArrow: "<img src='../images/head/Line-right.png' class='next' alt=''>",
   });
