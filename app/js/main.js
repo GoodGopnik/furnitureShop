1f0e4e92691a757d927514df206a94fd9b698dbd
GoodGopnik <https://github.com/GoodGopnik>
2021-03-03 14:57:01 +0200

    Сделал блок 4 и работаю над адаптивом

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,23 @@ $('.variable-width').slick({
     prevArrow: "<img src='../images/head/Line-left.png' class='prev' alt=''>",
     nextArrow: "<img src='../images/head/Line-right.png' class='next' alt=''>",
   });
+
+  $('.center').slick({
+    dots: true,
+    infinite: true,
+    // speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+    slidesToShow: 2,
+    // centerMode: true,
+    // variableWidth: false,
+    // // centerPadding: '100px',
+    // slidesToShow: 1,
+    prevArrow: false,
+    nextArrow: "<img src='../images/block4/Line.png' class='prev' alt=''>",
+    
+    // centerMode: true,
+    // infinite: false,
+    // dots: true,
+    // slidesToShow: 3,
+  });
